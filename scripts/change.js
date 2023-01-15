499ac71b5bd4b94c32c0d2c85ed02f60ec5e1289
Carlos Daniel Conchas Ortíz <carlosdanielconchasortiz@gmail.com>
2023-01-15 12:29:18 -0600

    Mejoras en el funcionamiento de la página Web

--- scripts/change.js
+++ scripts/change.js
@@ -5,19 +5,22 @@ const btnCopy = document.querySelector("#copy-btn");
 const copyRegion = document.querySelector("#copy-region");
 
 const enableCopy = () => {
-    btnCopy.classList.toggle("change");
-    copyRegion.classList.toggle("change");
-    tituloCopiar.classList.toggle("change");
-    imagenBuscar.classList.toggle("change");
+  btnCopy.classList.toggle("change");
+  copyRegion.classList.toggle("change");
+  tituloCopiar.classList.toggle("change");
+  imagenBuscar.classList.toggle("change");
 };
 
 const clearContent = () => {
+  if (texto.value) {
     texto.value = "";
-    copyParagraph.innerText = "Ingresa el texto que desees encriptar o desencriptar.";
+    copyParagraph.innerText =
+      "Ingresa el texto que desees encriptar o desencriptar.";
     btnCopy.classList.toggle("change");
     copyRegion.classList.toggle("change");
     tituloCopiar.classList.toggle("change");
     imagenBuscar.classList.toggle("change");
+  }
 };
 
 imagenLimpiar.onclick = clearContent;
